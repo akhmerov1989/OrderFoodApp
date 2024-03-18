f32a62a14c9ad5e53563b778fa01a8dacfeccf2a
akhmerov1989 <sergeyakhmerov@gmail.com>
2024-03-18 10:48:35 -0400

    Added new components

--- frontend/src/components/ui/Header.tsx
+++ frontend/src/components/ui/Header.tsx
@@ -1,20 +1,29 @@
 
 
-import { Link } from 'react-router-dom'; // Make sure to import Link from react-router-dom
+import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
 
 const Header = () => {
-    return (
-        <div className="border-b-2-orange-500 py-6">
-            <div className="container mx-auto flex justify-between items-center">
-                <Link to="/" className="text-3xl font-bold tracking-tight text-orange-500">
-                    QuickBite.com
-                </Link>
-            </div>
+  return (
+    <div className="border-b-2 border-b-orange-500 py-6">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link
+          to="/"
+          className="text-3xl font-bold tracking-tight text-orange-500"
+        >
+          MernEats.com
+        </Link>
+        <div className="md:hidden">
+          <MobileNav />
         </div>
-    );
+        <div className="hidden md:block">
+          <MainNav />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
-
-
 
