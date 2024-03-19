6dbc1b0cee1f75fdf8efa09ab9d7241e29e07ffd
akhmerov1989 <sergeyakhmerov@gmail.com>
2024-03-19 13:21:32 -0400

    Added HomePage, Footer

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,5 +1,7 @@
 
+import Hero from "@/components/ui/Hero";
 import Header from "@/components/ui/Header";
+import Footer from "@/components/Footer";
 
 type Props = {
   children: React.ReactNode;
@@ -9,8 +11,9 @@ const Layout = ({ children}: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      
+      <Hero />
       <div className="container mx-auto flex-1 py-10">{children}</div>
+      <Footer />
     </div>
   );
 };
