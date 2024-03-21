c555118f8f8c91263ca69f1b3b970180335ed7e5
akhmerov1989 <sergeyakhmerov@gmail.com>
2024-03-21 15:37:33 -0400

    Added UserNameMenu.tsx, logout

---
+++ frontend/src/components/UserNameMenu.tsx
@@ -0,0 +1,35 @@
+import { useAuth0 } from "@auth0/auth0-react"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
+import { CircleUserRound } from "lucide-react"
+import { DropdownMenuItem } from "./ui/dropdown-menu";
+import { Separator } from "@radix-ui/react-separator";
+import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
+
+const UserNameMenu = () => {
+    const { user, logout } = useAuth0();
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger className="flex items-center px-3 font-bold hover:text-oramge-500 gap-2">
+                <CircleUserRound className="text-orange-500" />
+                {user?.email}
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem>
+                    <Link to="/user-profile" className="font-bold hover:text-orange-500">
+                     User Profile 
+                    </Link>
+               </DropdownMenuItem>
+               <Separator />
+               <DropdownMenuItem>
+                    <Button onClick={() => logout()} className="flex flxe-1 font-bold bg-orange-500">
+                        Log out 
+                    </Button>
+                </DropdownMenuItem> 
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
+
+export default UserNameMenu;
